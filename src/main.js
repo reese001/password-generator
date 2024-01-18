2a0649a5e4bb67d4e32694ee34915ceb54796e64
reese parsons <reeseparsons99@gmail.com>
2024-01-18 18:11:00 -0400

    final product

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import "@fortawesome/fontawesome-free/js/solid.min.js";
 // importing the sass stylesheet for bundling
 import "./../sass/styles.scss";
 
+const NUMBER_OF_PASSWORDS = 20;
+
 let glyphs = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "@", "#", "$", "%", "^", "&", "*"];
 
 function randomMe(lower = 1, upper = 100) {
@@ -14,34 +16,41 @@ function randomMe(lower = 1, upper = 100) {
     return randomNum;
 }
 
-// this function generates random passwords based on arguments and returns array of passwords
-// length = generated password number of characters
-// specialChars = can the password include special characters? (true / false)
 function generatePasswords(length, specialChars) {
-    // let passwords = new Array();
-    // passwords[0] = "password";
-    // passwords[1] = "1234";
-    // passwords[2] = "740317";
-    
-    // let passwords = ["password", "1234", "740317"];
-    // passwords.push("123456789");
-    
-    // console.log(passwords);
-    
-    
+    let specialCharList = ["!", "@", "#", "$", "%", "^", "&", "*"];
     let passwords = [];
 
-    let password = "";
-    for (let n=0; n<length; n++) {
-        let glyph = glyphs[randomMe(0, glyphs.length - 1)];
-        password = password + glyph;
+    for (let p = 0; p < NUMBER_OF_PASSWORDS; p++) {
+        let password = "";
+        let validGlyphs;
+        if (specialChars) {
+            validGlyphs = glyphs;
+        } else {
+            validGlyphs = glyphs.filter(glyph => !specialCharList.includes(glyph));
+           
+        }
+        for (let n = 0; n < length; n++) {
+            let glyph = validGlyphs[randomMe(0, validGlyphs.length - 1)];
+        
+            
+            if (glyph >= "a" && glyph <= "z") {
+                glyph = randomMe(1, 2) === 1 ? glyph.toUpperCase() : glyph.toLowerCase();
+            }
+            password += glyph;
+        }
+        passwords.push(password);
     }
-
+    return passwords;
 }
 
 function main() {
+    let passwords = generatePasswords(10, false); 
+    let output = "";
+    for (let password of passwords) {
+        output += password + "<br>";
+    }
 
-    generatePasswords(10, true);
+    document.getElementById("passwords").innerHTML = output;
 }
 
 main();
