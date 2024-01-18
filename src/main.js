ea65eadae5828fd3c4c6cdefb75b4289b6bf8a0a
reese parsons <reeseparsons99@gmail.com>
2024-01-18 11:40:18 -0400

    added glyphs to array

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
+// importing font awesome
+// import "@fortawesome/fontawesome-free/scss/fontawesome.scss";
+// import "@fortawesome/fontawesome-free/scss/solid.scss";
+import "@fortawesome/fontawesome-free/js/fontawesome.min.js";
+import "@fortawesome/fontawesome-free/js/solid.min.js";
+
 // importing the sass stylesheet for bundling
 import "./../sass/styles.scss";
+
+let glyphs = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "@", "#", "$", "%", "^", "&", "*"];
 
 function randomMe(lower = 1, upper = 100) {
     let randomNum = Math.floor(Math.random() * (upper - lower + 1)) + lower;
@@ -10,11 +18,24 @@ function randomMe(lower = 1, upper = 100) {
 // length = generated password number of characters
 // specialChars = can the password include special characters? (true / false)
 function generatePasswords(length, specialChars) {
+    // let passwords = new Array();
+    // passwords[0] = "password";
+    // passwords[1] = "1234";
+    // passwords[2] = "740317";
+    
+    // let passwords = ["password", "1234", "740317"];
+    // passwords.push("123456789");
+    
+    // console.log(passwords);
+    
+    
+    let passwords = [];
 
 }
 
 function main() {
 
+    generatePasswords(10, true);
 }
 
 main();
